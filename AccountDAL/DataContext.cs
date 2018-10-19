e5989948e3075fcfc605fbdeadcb486b3bcc3b27
hayroz <30577058+hayroz@users.noreply.github.com>
2018-10-19 18:47:22 +0530

    Add files via upload

---
+++ AccountDAL/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using subDomain.Models;
+
+namespace WebApplication1.AccountDAL
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base(subDomain.Helpers.Helpers.GetUserManagementDb())
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
